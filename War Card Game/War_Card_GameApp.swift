f16304f7678ceba6defcb381a02eadc9a68a9b8a
ajmalpoovanath <ajmal.poovanath@gmail.com>
2024-07-09 20:13:27 +0530

    UI completed

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Ajmal p on 20/06/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
